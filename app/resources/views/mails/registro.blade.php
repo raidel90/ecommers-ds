d1e90f73ca686d1011fba8aa65dc182a41e42626
rvalle <rvalle@antel.net.uy>
2020-07-16 00:51:39 -0300

    correo registro estilo 1

---
+++ app/resources/views/mails/registro.blade.php
@@ -0,0 +1,73 @@
+<div align='center'>
+<div class=""  style="
+            background-color: #377e97; 
+            width:60%; 
+            color:white;
+            padding:40px;
+            margin:40px;
+
+            height: 80px;
+            text-shadows:30px;
+            ">
+<table>
+<tr>
+
+<td>
+<img src="assets/images/xycdi11704.png" alt="">
+</td>
+
+<td>
+
+<h2 style="color:#f4eeef;    
+text-shadow: 2px 1px 3px black;
+"   
+   >Gracias por registrarte,</br> Antes de continuar, debes configurar tu cuenta!</h2>
+
+
+</td>
+
+</tr>
+
+</table>
+
+
+</div>
+
+
+<h2>Por Favor confirme su suscripción.</h2>
+
+<a href="" style="font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';box-sizing:border-box;border-radius:7px;color:#fff;display:inline-block;text-decoration:none;background-color:#3490dc;border-top:10px solid #3490dc;border-right:18px solid #3490dc;border-bottom:10px solid #3490dc;border-left:18px solid #3490dc" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://cgabjbj.r.bh.d.sendibt3.com/tr/cl/gabnPX8_XQD-3-Q3EZDxC7iP0Pl26WeJvn84LzMcpArXU--23ILgS3dcbtfNT_p00kjVOlpTASqQ1YXkjTlkog95f5tMr0lNRVGJGYN9BYGbFIjY6_J5PXLfKk-OL4HjYSkK4Eam-Bqq317BsqsxfZdDE8NA7jDS0JMSsizcX84xB7W3F95RGOrqGTJHcvqmtgO1SuZS1zPamAmnVyzhJphkm8h-Ka93EPz-x1olz76L8ZiwcteRiPwUIqGlhyywpZlOhJM-IZYzLnZ0GJ-ZEeeXB-9tRdA5q1ML&amp;source=gmail&amp;ust=1594945939423000&amp;usg=AFQjCNHp8GQethfJvApf4wWfx_WB2F0mAg">
+
+    Confirmar tu cuenta
+    
+</a>
+
+<div align='left' style="margin:80px; margin-left:33%;">
+<h4>Muchas gracias por utilizar nuestra aplicación!</h4>
+<h4>Saludos,</h4>
+<h4>ExpressNutrition</h4>
+</div>
+
+
+<div style=" width:61%; ">
+Si tienes problemas haciendo click en el botón "Confirmar tu cuenta", copia y pega el siguiente enlace en tu navegador: <a href="url"></a>
+
+</div>
+
+
+<div class="footer" style="
+  background-color: #e6e3e3; 
+            width:62%; 
+            margin:40px;
+            padding:40px;
+
+            height: 30px;
+            text-shadows:30px;
+
+">
+
+© 2020 ExpressNutrition. All rights reserved.
+
+
+</div>
+</div>
